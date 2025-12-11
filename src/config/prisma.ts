83cdca669d8407ef01e6910c4453f4cb2ed29222
Anurag Sahu <as3305100@gmail.com>
2025-12-11 23:26:16 +0530

    feat: add prisma new version 7

---
+++ src/config/prisma.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { config } from "./config";
+
+const connectionString = config.database.url;
+
+const adapter = new PrismaPg({
+  connectionString,
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
+export default prisma;
